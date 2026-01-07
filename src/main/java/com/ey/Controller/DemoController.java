cdb575d0111d39b06e3c52fa9e4471fdd8a5765e
ArchanaaS71 <asamanikafalen@gmail.com>
2026-01-07 12:28:14 +0530

    first commit

---
+++ src/main/java/com/ey/Controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.ey.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	@GetMapping
+	public String home() {
+		return "Welcome Home";
+	}
+
+}
